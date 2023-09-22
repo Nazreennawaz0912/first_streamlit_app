6117e49eb1bd0819a4eafa8cc97fa718731e0abe
Nazreennawaz0912 <145743131+Nazreennawaz0912@users.noreply.github.com>
2023-09-22 11:37:10 +0530

    Update streamlit_app.py
    Added headers and text for the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega & Blueberry meal')
+streamlit.text('Spinach & rocket smoothie')
+streamlit.text('Hard biled egg')
