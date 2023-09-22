605dfab4d027d6cb081c77607372894d06bcf064
Nazreennawaz0912 <145743131+Nazreennawaz0912@users.noreply.github.com>
2023-09-22 11:46:41 +0530

    Update streamlit_app.py
    Added Special menu and added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,9 @@ import streamlit
 streamlit.title('My Parents New Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega & Blueberry meal')
-streamlit.text('Spinach & rocket smoothie')
-streamlit.text('Hard biled egg')
+streamlit.text('🥗 Spinach & rocket smoothie')
+streamlit.text('🐔 Hard biled egg')
+streamlit.text(' 🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
